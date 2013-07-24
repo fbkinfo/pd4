ad44803eb8607521fb691bfe239176437fbb530b
Ivan Martynov <ivanborisych@gmail.com>
2013-07-24 19:14:03 +0400

    Minor changes commit

--- app/assets/javascripts/donate.js
+++ app/assets/javascripts/donate.js
@@ -4,4 +4,18 @@ $(function(){
     e.preventDefault();
     window.print();
   });
+  $("#pd4_form").submit(function() {
+    filled = 
+      $("#name").val()    &&
+      $("#bdate").val()   &&
+      $("#passp").val()   &&
+      $("#address").val() &&
+      $("#citizen").val() &&
+      $("#amount").val();
+      if(!filled) {
+          alert("Заполните, пожалуйста, все поля.");
+          return false;
+      } else
+        return true;
+  });
 });
