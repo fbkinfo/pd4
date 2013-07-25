1aff896e08909126d6c1c7648191c1bba940c2d9
Ivan Martynov <ivanborisych@gmail.com>
2013-07-25 16:35:18 +0400

    Added js validation and checkbox for 'citizen' field

--- app/assets/javascripts/donate.js
+++ app/assets/javascripts/donate.js
@@ -1,10 +1,16 @@
 $(function(){
+
   $("#name").focus();
+
+  $("#citizen").prettyCheckable();
+
   $("#btn-print").click(function(e) {
     e.preventDefault();
     window.print();
   });
+
   $("#pd4_form").submit(function() {
+
     filled = 
       $("#name").val()    &&
       $("#bdate").val()   &&
@@ -13,9 +19,45 @@ $(function(){
       $("#citizen").val() &&
       $("#amount").val();
       if(!filled) {
-          alert("Заполните, пожалуйста, все поля.");
-          return false;
-      } else
-        return true;
+        alert("Заполните, пожалуйста, все поля.");
+        return false;
+      }
+    
+    if(!(/^[А-Яа-я]+ [А-Яа-я]+ [А-Яа-я]+$/.test($("#name").val()))) {
+      alert("Поле 'Фамилия Имя Отчество' должно содержать 3 слова, состоящих из русских букв и разделенных пробелами.");
+      $("#name").focus();
+      return false;
+    }
+    
+    if(!(/^\d{2}\.\d{2}\.\d{4}$/.test($("#bdate").val()))) {
+      alert("Поле 'Дата рождения' должно содержать дату в формате ДД.ММ.ГГГГ");
+      $("#bdate").focus();
+      return false;
+    }
+      
+    if(!(/^\d{4} \d{6}$/.test($("#passp").val()))) {
+      alert("Поле 'Паспорт: серия номер' должно содержать серию из 4 цифр и номер из 6 цифр. Серия и номер должны быть разделены пробелом.");
+      $("#passp").focus();
+      return false;
+    }
+      
+    if(!(/^[А-Яа-я0-9\.\-\/ ]+$/.test($("#address").val()))) {
+      alert("Поле 'Адрес' может содержать только русские буквы, пробелы, а так же точку, тире и знак дроби '/'.");
+      $("#address").focus();
+      return false;
+    }
+      
+    if(!(/^\d+$/.test($("#amount").val()))) {
+      alert("Поле 'Сумма пожертвования в рублях' может содержать только целое число.");
+      $("#amount").focus();
+      return false;
+    }
+    
+    if(!$("#citizen").prop('checked')) {
+      alert("Добровольные пожертвования в избирательный фонд принимаются только от граждан Российской Федерации.")
+      return false;
+    }
+      
   });
+
 });
